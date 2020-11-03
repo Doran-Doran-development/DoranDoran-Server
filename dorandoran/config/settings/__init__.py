aa85146437afc0cff547c69be73fa9d8041a50e6
hanbin8269 <hanbin8269@gmail.com>
2020-11-03 11:00:22 +0900

    Change file structure

---
+++ dorandoran/config/settings/__init__.py
@@ -0,0 +1,6 @@
+import os
+
+SETTINGS_MODULE = os.environ.get("DJANGO_SETTINGS_MODULE") 
+# 환경 변수에서 DJANGO_SETTINGS_MODULE을 지정해주지 않으면 .local 파일을 임포트 해준다
+if not SETTINGS_MODULE or SETTINGS_MODULE == "config.settings":
+    from .local import *
--- dorandoran/config/settings/base.py
+++ dorandoran/config/settings/base.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'dorandoran.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'dorandoran.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
