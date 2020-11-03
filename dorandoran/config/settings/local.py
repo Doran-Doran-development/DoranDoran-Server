731099318912f112d1e1cac97bfeaef7ee338d7e
hanbin8269 <hanbin8269@gmail.com>
2020-11-03 15:47:05 +0900

    Change settings.local.py

--- dorandoran/config/settings/local.py
+++ dorandoran/config/settings/local.py
@@ -1,3 +1,9 @@
 from .base import *
 
 # 로컬환경의 추가 설정을 넣어주자!
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
