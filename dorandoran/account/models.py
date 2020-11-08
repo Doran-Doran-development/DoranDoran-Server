1541fcc2eb9e1520bf089451ac82b54f72ef9474
hanbin8269 <hanbin8269@gmail.com>
2020-11-08 23:42:19 +0900

    Feature User model

--- dorandoran/account/models.py
+++ dorandoran/account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    username = models.CharField(
+        _('username'),
+        max_length=150,
+        help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
+    )
+    email = models.EmailField(_('email address'), unique=True, max_length=128)
+    
+    USERNAME_FIELD = 'email'
