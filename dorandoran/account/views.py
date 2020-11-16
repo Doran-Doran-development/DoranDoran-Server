e71d0b4c805ec05a779b6809bbea7d838d196e1d
hanbin8269 <hanbin8269@gmail.com>
2020-11-16 20:32:21 +0900

    Feature Login view

--- dorandoran/account/views.py
+++ dorandoran/account/views.py
@@ -1,8 +1,23 @@
-from rest_framework import generics
-from .serializers import CreateUserSerializer, LoginUserSerializer
+from rest_framework import generics, status
+from .serializers import CreateUserSerializer, LoginUserSerializer, UserSerializer
 from .models import User
+from rest_framework.response import Response
 
 
 class RegistrationView(generics.CreateAPIView):
     serializer_class = CreateUserSerializer
     queryset = User.objects.all()
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginUserSerializer
+    queryset = User.objects.all()
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data) # serializer화 시키고
+        serializer.is_valid() # is_valid로 확인 하고
+        user,token = serializer.validated_data # serializer_data 받아서 리턴
+        return Response({
+            "user" : UserSerializer(user).data,
+            "token" : token
+        }, status = status.HTTP_200_OK)
