5a3176ee2979d7760bfff82aac4e65ba0541524a
hanbin8269 <hanbin8269@gmail.com>
2020-11-16 17:55:43 +0900

    Feature LoginSerializer

--- dorandoran/account/serializers.py
+++ dorandoran/account/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import User
-
+from django.contrib.auth import authenticate
+from rest_framework_jwt import utils
+import jwt
 
 class CreateUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +12,24 @@ class CreateUserSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+
+class LoginUserSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=128)
+    password = serializers.CharField()
+    
+    def validate(self, data):
+        credentials = {
+            "email" : data["email"],
+            "password" : data["password"]
+        }
+        user = authenticate(**credentials) # backend.authenticate 쓰고
+
+        payload = {
+            "email" : user.email,
+            "is_teacher" : user.is_teacher,
+            "is_active" : user.is_active
+        }
+        token = utils.jwt_encode_handler(payload) # token 만들고
+        
+        return user,token # user, token 반환
