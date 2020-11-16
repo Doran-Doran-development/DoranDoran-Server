28832eca4f1c069d6e7b7d192b14de49d1d32854
hanbin8269 <hanbin8269@gmail.com>
2020-11-16 20:30:24 +0900

    Fix Couldn't Find UserBackend in authenticate method

--- dorandoran/account/backends.py
+++ dorandoran/account/backends.py
@@ -8,17 +8,17 @@ class UserBackend(BaseBackend):
     """
     settings.AUTH_USER_MODEL 셋팅에 의존한다.
     """
-    def authenticate(self, email = None, password = None, *args, **kwargs):
+    def authenticate(self, credentials):
+        email = credentials['email']
+        password = credentials['password']
         if email is None: 
             email = kwargs.get(UserModel.USERNAME_FIELD)
         try: # email에 매치되는 인스턴스 존재하는지 확인
             user = UserModel.objects.get(email = email)
         except UserModel.DoesNotExist:
             return
-        
         if user.check_password(password) and self.user_can_authenticate(user): # password랑 맞는지 체크, is_active 인지 체크
             return user
-        # password랑 맞는지 체크 3
 
     def user_can_authenticate(self, user):
         return user.is_active
