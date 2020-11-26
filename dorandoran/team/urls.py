100556c77ac24e8b77ee2c21b9cc9f45fdc03ab1
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-11-27 00:28:51 +0900

    ADD update API rule

--- dorandoran/team/urls.py
+++ dorandoran/team/urls.py
@@ -1,1 +1,10 @@
-from django.urls import path
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+from .views import TeamViewSet
+
+router = DefaultRouter()
+router.register(r"", TeamViewSet, basename="team")
+
+urlpatterns = [
+    url(r"^", include(router.urls)),
+]
