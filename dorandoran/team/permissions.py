d42ff6f5d5e6b79c4b2b2fbeec489621e882d3c7
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-11-27 22:58:01 +0900

    CREATE permission function is_teacher, is_project name

--- dorandoran/team/permissions.py
+++ dorandoran/team/permissions.py
@@ -1,5 +1,24 @@
+import re
 from rest_framework.permissions import BasePermission
+from rest_framework.exceptions import ValidationError
+from account.models import User
 
-class IsTeacher(BasePermission):
-    def has_permission(self, request, view):
-        return super().has_permission(request, view)
+
+class TeamPermission(BasePermission):
+    def is_teacher(self, value):
+        try:
+            queryset = User.objects.get(email=value)
+
+            if not queryset["is_teacher"]:
+                return False
+
+        except User.DoesNotExist:
+            return
+
+        return True
+
+    def is_valid_project_name(self, value):
+        teamname_format = re.compile("\B-\B")
+        if not teamname_format.search(value):
+            return False
+        return True
