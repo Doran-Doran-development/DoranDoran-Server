6458c5fc4d623cc8868cf2b7d897bd442ddbb0e1
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-11-27 22:58:32 +0900

    CREATE Team serailizer

--- dorandoran/team/serializers.py
+++ dorandoran/team/serializers.py
@@ -1,17 +1,36 @@
 import re
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, Serializer
+from django.utils.translation import ugettext as _
 from .models import Team, LinkedTeamUser
 from rest_framework.exceptions import ValidationError
+from .permissions.TeamPermission import is_teacher, is_valid_project_name
 
 
 class TeamSerializer(ModelSerializer):
     class Meta:
         model = Team
+        fields = ("team_id", "project", "description", "teacher")
 
-    def is_valid_project(self, value):
-        p = re.compile("\B-\B")
-        if not p.search(value):
-            return ValidationError("대회명-팀명 형식에 맞추어야 합니다.")
+    def validate(self, req):
+        teacher = req["teacher"]
+        project = req["project"]
+
+        is_valid_teacher = is_teacher(teacher)
+        if is_teacher == None:
+            msg = _("User instance not exists")
+            raise ValidationError(msg)
+
+        if not is_teacher:
+            msg = _("{} is not teacher".format(teacher))
+            raise ValidationError(msg)
+
+        is_valid_project = is_valid_project_name(project)
+
+        if not is_valid_project:
+            msg = _("is not valid project name format")
+            raise ValidationError(msg)
+
+        return True
 
 
 class LinkedTeamUserSerializer(ModelSerializer):
