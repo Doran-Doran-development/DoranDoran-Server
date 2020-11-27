a8dc9db0f5ed22570de37ad301bdaa55d8158341
seojinsarejj <s19009@naver.com>
2020-11-27 11:30:20 +0900

    Feature room model

---
+++ dorandoran/room/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=255)
+    max_team = models.IntegerField()
+    owner = models.ForeignKey('user.User',on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    
