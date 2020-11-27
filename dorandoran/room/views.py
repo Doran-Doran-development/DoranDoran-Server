28ef818f541a76a5c1c40945663224beea590609
seojinsarejj <s19009@naver.com>
2020-11-27 17:50:32 +0900

    Feature create ListRoomAPI

--- dorandoran/room/views.py
+++ dorandoran/room/views.py
@@ -1,2 +1,19 @@
-from django.shortcuts import render
+import jwt
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics
+from rest_framework import mixins
+from .models import Room
+from .serializers import RoomSerializer
+
+
+
+class ListRoomAPI(mixins.ListModelMixins
+                  generics.GenericsAPIView):
+
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+    def get(self,request,*args,**kwargs):
+        return self.list(reqeust,*args,**kwargs)
 
