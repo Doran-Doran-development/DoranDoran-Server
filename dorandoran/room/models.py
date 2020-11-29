91f1cc7a352ba7a98495b46165ca8f0b22c9b43e
seojinsarejj <s19009@naver.com>
2020-11-29 17:19:17 +0900

    Feature table name set

--- dorandoran/room/models.py
+++ dorandoran/room/models.py
@@ -4,8 +4,9 @@ from django.db import models
 class Room(models.Model):
     name = models.CharField(max_length=255)
     max_team = models.IntegerField()
-    owner = models.ForeignKey('account.User',on_delete = models.CASCADE)
-    status = models.BooleanField(default = True)
-    created_at = models.DateTimeField(auto_now_add = True)
+    owner = models.ForeignKey("account.User", on_delete=models.CASCADE)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
-    
+    class Meta:
+        db_table = "room"
