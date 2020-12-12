5241c42e0e869d7c424d84f4a81025495e23ccd6
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-12 22:34:40 +0900

    Refactor validate func

--- dorandoran/team/serializers.py
+++ dorandoran/team/serializers.py
@@ -2,6 +2,7 @@ import re
 from rest_framework import serializers
 from django.utils.translation import ugettext as _
 from .models import Team, LinkedTeamUser
+from account.models import User
 from rest_framework.exceptions import ValidationError
 from .permissions import TeamPermission
 
@@ -12,24 +13,37 @@ class TeamSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
     def validate(self, req):
-        teacher = req["teacher"]
+
+        teacher = req["is_teacher"]
         project = req["project"]
 
-        is_valid_teacher = TeamPermission.is_teacher(teacher)
-        if is_valid_teacher == None:
+        #유효한 교사 이메일인지 검사
+        is_valid_teacher = self.is_teacher(teacher)
+        if not is_valid_teacher:
             msg = _("User instance not exists")
             raise ValidationError(msg)
 
-        is_valid_project = TeamPermission.is_valid_project_name(project)
-
+        #프로젝트 이름이 형식에 맞는지 검사
+        is_valid_project = self.is_valid_project_name(project)
         if not is_valid_project:
             msg = _("is not valid project name format")
             raise ValidationError(msg)
 
-        return req
+        return True
 
+    def is_teacher(self, email):
+        queryset = User.objects.filter(email=email).filter(is_teacher=True)
+        if not queryset.exists():
+            return False
+        return True
+
+    def is_valid_project_name(self, project):
+        project_format = re.compile("(.+)[-](.+)")
+        if not project_format.search(project__name):
+            return False
+        return True
 
 class LinkedTeamUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = LinkedTeamUser
-        fields = ("team_id", "email")
+        fields = "__all__"
