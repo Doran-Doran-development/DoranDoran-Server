c9e641f69db2a57fb45859041e0be39f8389adb8
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-13 18:32:32 +0900

    Feature route MemberViewSet

--- dorandoran/team/urls.py
+++ dorandoran/team/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url, include
 from django.urls import path
 from rest_framework.routers import DefaultRouter
-from .views import TeamViewSet, ReadOnlyTeamViewSet
+from .views import TeamViewSet, ReadOnlyTeamViewSet, MemberViewSet
 
 router = DefaultRouter()
 router.register(r"", TeamViewSet, basename="team")
 router.register(r"show", ReadOnlyTeamViewSet, basename="get_team")
+router.register(r"member", MemberViewSet, basename="member")
 
 urlpatterns = [
     url("", include(router.urls)),
