b6be118fd5a17da6b8b535f513e4d0a163e77c02
hanbin8269 <hanbin8269@gmail.com>
2020-12-14 16:02:59 +0900

    Create ReservationQueue model

--- dorandoran/reserve/models.py
+++ dorandoran/reserve/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class ReservationQueue(models.Model):
+    room_id = models.ForeignKey("room.Room", on_delete=models.CASCADE)
+    team_id = models.ForeignKey("team.Team", on_delete=models.CASCADE)
+    status = models.IntegerField(default=0)
+    reserve_time = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = u"reservation_queue"
+    
+    def __str__:
+        return team_id + "팀의 " +reserve_time + "교시 예약 요청"
