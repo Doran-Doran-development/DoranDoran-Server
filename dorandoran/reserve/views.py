db719da3daf02979c0e60c55cdb5392a9d09f744
hanbin8269 <hanbin8269@gmail.com>
2020-12-15 16:07:01 +0900

    Create reservation create view

--- dorandoran/reserve/views.py
+++ dorandoran/reserve/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from account.authentication import CustomJSONWebTokenAuthentication
 from .serializers import ReservationQueueSerializer
+from account.serializers import UserSerializer
 from .models import ReservationQueue
 from account.models import User
 
@@ -9,14 +10,16 @@ from account.models import User
 class ReservationViewSet(viewsets.ModelViewSet):
     authentication_classes = [CustomJSONWebTokenAuthentication]
     serializer_class = ReservationQueueSerializer
+    queryset = ReservationQueue.objects.all()
 
-    def create(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):  # 학생만 가능
         serializer = ReservationQueueSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+
         instance_user = User.objects.get(email=request.user.email)
         serializer.save(reserver_id=instance_user)
-        print(serializer.validated_data)
-        return Response(serializer.validated_data)
+
+        return Response(serializer.data)
 
     # GET 요청은 모두 가능
     # DELETE 글 주인만 가능
