0600909a368b9a102e99186955395275300e8463
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-15 23:16:51 +0900

    Etc init migrations

--- dorandoran/room/permissions.py
+++ dorandoran/room/permissions.py
@@ -6,5 +6,4 @@ class IsTeacherOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-
         return bool(request.user.is_teacher)
