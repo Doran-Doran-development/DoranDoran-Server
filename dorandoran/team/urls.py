83a55ed8d5bd0a8a52e928f0a30a5c596113cc19
ByunWoungSub <50494659+ByunWoungSub@users.noreply.github.com>
2020-12-15 23:11:16 +0900

    Fix add ^ at router prefix

--- dorandoran/team/urls.py
+++ dorandoran/team/urls.py
@@ -1,13 +1,11 @@
-from django.conf.urls import url, include
-from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import TeamViewSet, ReadOnlyTeamViewSet, MemberViewSet
 
-router = DefaultRouter()
-router.register(r"", TeamViewSet, basename="team")
-router.register(r"show", ReadOnlyTeamViewSet, basename="get_team")
+router = DefaultRouter(trailing_slash=False)
+router.register(r"^", TeamViewSet, basename="team")
 router.register(r"member", MemberViewSet, basename="member")
+router.register(r"show", ReadOnlyTeamViewSet, basename="get_team")
 
-urlpatterns = [
-    url("", include(router.urls)),
-]
+urlpatterns = router.urls
