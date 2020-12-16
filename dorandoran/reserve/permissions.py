93fd67ca82c28fe239894a4284ccf79b6555293d
hanbin8269 <hanbin8269@gmail.com>
2020-12-16 16:56:24 +0900

    Change view.action name

--- dorandoran/reserve/permissions.py
+++ dorandoran/reserve/permissions.py
@@ -10,10 +10,9 @@ class ReservePermission(BasePermission):
     def has_permission(self, request, view):
         if request.user.role == 1:  # Admin 유저는 모든 사용권한을 가진다
             return True
-        print(view.action)
         if view.action in ("list", "retrieve"):
             return True
-        elif view.action == "post":  # 학생 일때 가능
+        elif view.action == "create":  # 학생 일때 가능
             return request.user.role == 3
         elif view.action == "partial_update":  # 선생님 일때 가능
             return request.user.role == 2
