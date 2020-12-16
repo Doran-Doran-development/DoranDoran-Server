245009ecc827d8574e5494afd164bc7497c9d793
hanbin8269 <hanbin8269@gmail.com>
2020-12-16 13:47:03 +0900

    Create ReservePermission

--- dorandoran/account/permissions.py
+++ dorandoran/account/permissions.py
@@ -10,16 +10,13 @@ class ReservePermission(BasePermission):
     def has_permission(self, request, view):
         if request.user.role == 1:  # Admin 유저는 모든 사용권한을 가진다
             return True
-
+        print(view.action)
         if view.action in ("list", "retrieve"):
             return True
         elif view.action == "post":  # 학생 일때 가능
             return request.user.role == 3
-        elif view.action == "patch":  # 선생님 일때 가능
+        elif view.action == "partial_update":  # 선생님 일때 가능
             return request.user.role == 2
-        elif view.action == "delete":
-            reserver_id = ReservationQueue.objects.get(
-                reserve_id=request.parser_context["kwargs"]["reserve_id"]
-            )
-            print(reserver_id)
-            return reserver_id
+        elif view.action == "destroy":  # 예약 주인 일때 가능
+            selected_queue = ReservationQueue.objects.get(id=request.parser_context["kwargs"]["pk"])
+            return request.user.email == str(selected_queue.reserver_id)
