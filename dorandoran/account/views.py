d7534e92eb670d0678cef990f94dc49bd1d431b4
hanbin8269 <hanbin8269@gmail.com>
2020-12-17 17:31:25 +0900

    Create RefreshJSONWebTokenView

--- dorandoran/account/views.py
+++ dorandoran/account/views.py
@@ -43,3 +43,18 @@ class SignOutView(generics.GenericAPIView):
         current_user = User.objects.get(email=request.user.email)
         current_user.delete()
         return Response(status=200)
+
+
+class RefreshJSONWebTokenView(generics.GenericAPIView):
+    serializer_class = RefreshJSONWebTokenSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)  # serializer화 시키고
+        if not serializer.is_valid():  # is_valid로 확인 하고
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        token = serializer.validated_data  # serializer_data 받아서 리턴
+        return Response(
+            {"success": True, "token": token},
+            status=status.HTTP_200_OK,
+        )
