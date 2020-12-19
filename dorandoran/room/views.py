cd956f1d1f4eaefbd291f3836157750073ae3fff
hanbin8269 <hanbin8269@gmail.com>
2020-12-20 01:34:51 +0900

    Fix to add current_team list in RooMViewSet's retrieve method

--- dorandoran/room/views.py
+++ dorandoran/room/views.py
@@ -8,6 +8,7 @@ from account.authentication import CustomJSONWebTokenAuthentication
 
 from .permissions import IsTeacherOrReadOnly
 from .models import Room
+from reserve.models import ReservationQueue
 from .serializers import RoomSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework import permissions
@@ -40,19 +41,32 @@ class RoomViewSet(viewsets.ViewSet):
             return Response(serializer.errors)
 
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data)
 
     def retrieve(self, request, pk=None):
         queryset = Room.objects.get(pk=pk)
         serializer = RoomSerializer(queryset)
-        return Response(serializer.data)
+        cur_team_count = []
 
-    def destroy(self, request,pk=None):
+        for i in range(8, 12):
+            accepted_reserve_count = (
+                ReservationQueue.objects.filter(reserve_time=i)
+                .filter(status=1)
+                .aggregate(Count("id"))
+            )["id__count"]
+            cur_team_count.append(accepted_reserve_count)
+
+        result = serializer.data
+        result["cur_team"] = cur_team_count
+        print(cur_team_count)
+        return Response(result)
+
+    def destroy(self, request, pk=None):
         instance = Room.objects.get(pk=pk)
         instance.delete()
-        return Response({"message":"Success"},status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": "Success"}, status=status.HTTP_204_NO_CONTENT)
 
-    def update(self, request,pk=None):
+    def update(self, request, pk=None):
         instance = Room.objects.get(pk=pk)
         instance.name = request.data["name"]
         instance.max_team = request.data["max_team"]
