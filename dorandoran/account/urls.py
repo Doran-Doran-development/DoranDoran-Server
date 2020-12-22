ce626f9bf6cb7057fc7dc6af168849f0bd3f9126
hanbin8269 <hanbin8269@gmail.com>
2020-12-22 13:11:38 +0900

    set router urls

--- dorandoran/account/urls.py
+++ dorandoran/account/urls.py
@@ -1,13 +1,20 @@
-from django.urls import path
+from django.urls import path, include
 from .views import (
     RegistrationView,
     LoginView,
     SignOutView,
     RefreshJSONWebTokenView,
     MyUserInfoView,
+    UserViewSet,
 )
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r"users", UserViewSet)
+
 urlpatterns = [
+    path("", include(router.urls)),
     path("sign-up", RegistrationView.as_view()),
     path("login", LoginView.as_view()),
     path("sign-out", SignOutView.as_view()),
