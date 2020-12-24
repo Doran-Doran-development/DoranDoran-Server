590a3471385480e8d278f7e929c7927063c0f18b
hanbin8269 <hanbin8269@gmail.com>
2020-12-24 23:32:20 +0900

    Fix Anonymous user exception

--- dorandoran/account/permissions.py
+++ dorandoran/account/permissions.py
@@ -3,7 +3,10 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwnerOrAdmin(BasePermission):
     def has_permission(self, request, view):
-        if request.user.role == 1:
-            return True
+        if not request.user.is_anonymous:
+            if request.user.role == 1:
+                return True
 
-        return request.user.uid == request.parser_context["kwargs"]["pk"]
+            return request.user.uid == request.parser_context["kwargs"]["pk"]
+
+        return False
