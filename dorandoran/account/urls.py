e7fe078fa02a9eb1f2fe94c56344fe852a9008af
hanbin8269 <hanbin8269@gmail.com>
2020-12-25 22:01:32 +0900

    delete old urls

--- dorandoran/account/urls.py
+++ dorandoran/account/urls.py
@@ -15,9 +15,6 @@ router.register(r"users", UserViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("sign-up", RegistrationView.as_view()),
     path("login", LoginView.as_view()),
-    path("sign-out", SignOutView.as_view()),
     path("refresh", RefreshJSONWebTokenView.as_view()),
-    path("my-info", MyUserInfoView.as_view()),
 ]
