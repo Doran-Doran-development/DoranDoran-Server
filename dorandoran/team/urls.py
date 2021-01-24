23c57e3973b12305cf2cbd1c0fb30041f4e23693
icefirebear <woungsub1234@gmail.com>
2021-01-24 20:20:12 +0900

    feature: register MemberViewset, TeamViewSet, TeamDetailView

--- dorandoran/team/urls.py
+++ dorandoran/team/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TeamViewSet
+from .views import TeamViewSet, MemberViewSet, TeamDetailView
 
 router = DefaultRouter()
-router.register(r'', TeamViewSet, basename="team")
+router.register(r"member", MemberViewSet, basename="member")
+router.register(r"", TeamViewSet, basename="team")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("", include(router.urls)),
+    path(r"detail/<int:pk>", TeamDetailView.as_view()),
+]
