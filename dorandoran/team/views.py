69be80c7556fe0dd97aeafea5a46f1a01d94870f
icefirebear <woungsub1234@gmail.com>
2021-01-24 20:21:17 +0900

    feature: feat TeamDetailView, update TeamViewSet, MemberViewSet

--- dorandoran/team/views.py
+++ dorandoran/team/views.py
@@ -3,6 +3,8 @@ from rest_framework import viewsets, generics, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import ValidationError
 from rest_framework.decorators import action
+import json
+from django.core import serializers
 
 from account.authentication import CustomJSONWebTokenAuthentication
 from .models import Team, LinkedTeamUser
@@ -12,34 +14,68 @@ from .permissions import isTeacherOrNotDelete
 
 # Create your views here.
 
-class TeamViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
+
+class TeamViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
     authentication_classes = [CustomJSONWebTokenAuthentication]
     permission_classes = [IsAuthenticated & isTeacherOrNotDelete]
 
+
+class TeamDetailView(generics.GenericAPIView):
+    authentication_classes = [CustomJSONWebTokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        team_with_member = Team.objects.raw(
+            """
+        SELECT 
+            T.team_id,
+            T.project,
+            T.description,
+            T.teacher_id,
+            (
+                SELECT
+                    uid_id
+                FROM
+                    linkedteamuser
+                WHERE
+                    team_id_id = 2
+            )
+            FROM
+                Team as T
+            WHERE
+                T.team_id = 2;
+        """.format(
+                self.kwargs["pk"]
+            )
+        )
+        # team_with_membe = Team.objects.prefetch_related(
+        #     Prefetch(
+        #         "linkedteamuser_set", queryset=LinkedTeamUser.objects.filter(team_id=pk)
+        #     )
+        # ).filter(team_id=pk)
+        data = serializers.serialize("json", team_with_member)
+        return data
+
+
+class MemberViewSet(
+    mixins.CreateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet
+):
+    queryset = LinkedTeamUser.objects.all()
+    serializer_class = LinkedTeamUserSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = [CustomJSONWebTokenAuthentication]
+
     @action(detail=True, methods=["get"])
     def detailed(self, request, pk=None):
-        team_obj = Team.objects.get(team_id=pk)
-        members_obj = LinkedTeamUser.objects.filter(team_id=pk)
-        print("team", team_obj)
-        print("member", members_obj)
-
-        team_serializer = TeamSerializer(data=team_obj)
-        members_serializer = LinkedTeamUserSerializer(data=members_obj, many=True)
-
-        if team_serializer.is_empty_team():
-            return Response(ValidationError("empty content"), status=204)
-        if not team_serializer.is_valid():
-            print("team error")
-            return Response(team_serializer.errors)
-
+        queryset = LinkedTeamUser.objects.filter(team_id=pk)
+        members_serializer = LinkedTeamUserSerializer(data=queryset, many=True)
         if not members_serializer.is_valid():
-            print("member error")
-            return Response(members_serializer.errors)
-
-        result_team_obj = team_serializer.validated_data
-        result_team_obj.update(members_serializer.data)
-        return Response(result_team_obj)
-
-
+            raise ValidationError(code=400)
+        return Response(members_serializer.data, status=200)
