88cbb962df433c0cd65e96239477c663d24dee3f
변웅섭 <50494659+icefirebear@users.noreply.github.com>
2021-01-31 20:36:02 +0900

    Update dorandoran/team/permissions.py
    Co-authored-by: hanbin8269 <58972963+hanbin8269@users.noreply.github.com>

--- dorandoran/team/permissions.py
+++ dorandoran/team/permissions.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions
 
-class isTeacherOrNotDelete(permissions.BasePermission):
+class IsTeacherOrNotDelete(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method == 'DELETE':
             return bool(request.user.role == 2)
