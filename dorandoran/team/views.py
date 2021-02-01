a7d27b0cd6165c69b08ab1b22c87fa1814f6040c
icefirebear <woungsub1234@gmail.com>
2021-02-01 23:37:59 +0900

    Refactor: refactor view

--- dorandoran/team/views.py
+++ dorandoran/team/views.py
@@ -9,7 +9,10 @@ import json
 import jwt
 from django.http import JsonResponse
 
-from account.authentication import CustomJSONWebTokenAuthentication, jwt_get_uid_from_payload_handler
+from account.authentication import (
+    CustomJSONWebTokenAuthentication,
+    jwt_get_uid_from_payload_handler,
+)
 from account.models import User
 from config.settings.dev import JWT_AUTH
 from .models import Team, LinkedTeamUser
@@ -43,35 +46,45 @@ class MemberViewSet(
     def create(self, request, *args, **kwargs):
         team_num = self.get_queryset().filter(team_id=request.data["team_id"])
         if len(team_num) > 12:
-            return Response("This team is already full.",status=status.HTTP_406_NOT_ACCEPTABLE)
+            return Response(
+                "This team is already full.", status=status.HTTP_406_NOT_ACCEPTABLE
+            )
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(
+            serializer.data, status=status.HTTP_201_CREATED, headers=headers
+        )
 
     @action(detail=True, methods=["get"])
     def detailed(self, request, pk=None):
         queryset = LinkedTeamUser.objects.filter(team_id=pk)
-
         if len(queryset) == 0:
-            return Response(status=status.HTTP_204_NO_CONTENT)
+            return Response(
+                "There's no matching info", status=status.HTTP_204_NO_CONTENT
+            )
         if len(queryset) == 1:
             member_obj = model_to_dict(queryset.first())
-
-            return JsonResponse(member_obj) 
+            return JsonResponse(member_obj)
         else:
             members_serializer = LinkedTeamUserSerializer(queryset, many=True)
 
         return Response(members_serializer.data, status=200)
 
     def destroy(self, request, *args, **kwargs):
-        token = request.headers.get('Authorization', None).split()[1]
-        payload = jwt.decode(token, JWT_AUTH["JWT_SECRET_KEY"], JWT_AUTH["JWT_ALGORITHM"])
+        token = request.headers.get("Authorization", None).split()[1]
+        payload = jwt.decode(
+            token, JWT_AUTH["JWT_SECRET_KEY"], JWT_AUTH["JWT_ALGORITHM"]
+        )
         token_uid = jwt_get_uid_from_payload_handler(payload)
         try:
-            instance = LinkedTeamUser.objects.get(team_id=self.kwargs["pk"], uid=token_uid)
+            instance = LinkedTeamUser.objects.get(
+                team_id=self.kwargs["pk"], uid=token_uid
+            )
         except Exception as e:
-            raise Http404("user not found")
+            return Response(
+                "There's no matching info", status=status.HTTP_404_NOT_FOUND
+            )
         self.perform_destroy(instance)
         return Response(status=status.HTTP_204_NO_CONTENT)
