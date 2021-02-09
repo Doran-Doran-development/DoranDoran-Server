cdfb63464f1f257ee0962deab9e9b2f605a5567d
hanbin8269 <hanbin8269@gmail.com>
2021-02-10 00:38:15 +0900

    chore : uid -> uuid로 변경

--- dorandoran/account/authentication.py
+++ dorandoran/account/authentication.py
@@ -6,8 +6,8 @@ from django.utils.translation import ugettext as _
 from .models import User
 
 
-def jwt_get_uid_from_payload_handler(payload):
-    return payload.get("uid")
+def jwt_get_uuid_from_payload_handler(payload):
+    return payload.get("uuid")
 
 
 class CustomJSONWebTokenAuthentication(JSONWebTokenAuthentication):
@@ -15,14 +15,14 @@ class CustomJSONWebTokenAuthentication(JSONWebTokenAuthentication):
         """
         Returns an active user that matches the payload's user id and uid.
         """
-        uid = jwt_get_uid_from_payload_handler(payload)
+        uuid = jwt_get_uuid_from_payload_handler(payload)
 
-        if not uid:
+        if not uuid:
             msg = _("Invalid payload.")
             raise exceptions.AuthenticationFailed(msg)
 
         try:
-            user = User.objects.get_by_natural_key(uid)
+            user = User.objects.get_by_natural_key(uuid)
         except User.DoesNotExist:
             msg = _("Invalid signature.")
             raise exceptions.AuthenticationFailed(msg)
