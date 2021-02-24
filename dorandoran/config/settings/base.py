0e609ee32759c6bd6d5807339b2a8e0ffb9762f4
hanbin8269 <hanbin8269@gmail.com>
2021-02-25 00:28:49 +0900

    [ADD] JWT 설정 추가

--- dorandoran/config/settings/base.py
+++ dorandoran/config/settings/base.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 import json
+from datetime import timedelta
 
 # ============== 경로 설정 =============
 _BASE = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -87,6 +88,32 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
+
+# ======= JWT 설정 =======
+JWT_AUTH = {
+    "JWT_PAYLOAD_GET_USERNAME_HANDLER": "account.utils.jwt_get_username_from_payload_handler",
+    "JWT_ENCODE_HANDLER": "rest_framework_jwt.utils.jwt_encode_handler",
+    "JWT_DECODE_HANDLER": "rest_framework_jwt.utils.jwt_decode_handler",
+    "JWT_PAYLOAD_HANDLER": "rest_framework_jwt.utils.jwt_payload_handler",
+    "JWT_PAYLOAD_GET_USER_ID_HANDLER": "rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler",
+    "JWT_RESPONSE_PAYLOAD_HANDLER": "rest_framework_jwt.utils.jwt_response_payload_handler",
+    "JWT_SECRET_KEY": config_secret_file["jwt"]["secret_key"],
+    "JWT_GET_USER_SECRET_KEY": None,
+    "JWT_PUBLIC_KEY": None,
+    "JWT_PRIVATE_KEY": None,
+    "JWT_ALGORITHM": config_secret_file["jwt"]["algorithm"],
+    "JWT_VERIFY": True,
+    "JWT_VERIFY_EXPIRATION": True,
+    "JWT_LEEWAY": 0,
+    "JWT_EXPIRATION_DELTA": timedelta(days=1),
+    "JWT_AUDIENCE": None,
+    "JWT_ISSUER": None,
+    "JWT_ALLOW_REFRESH": True,
+    "JWT_REFRESH_EXPIRATION_DELTA": timedelta(days=30),
+    "JWT_AUTH_HEADER_PREFIX": "jwt",
+    "JWT_AUTH_COOKIE": None,
+}
+# ========================
 
 ROOT_URLCONF = "config.urls"
 
